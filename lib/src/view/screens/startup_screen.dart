1d76282b66df9a4b436911d4da45867f310636a1
LucasRossatto <lucas.sant.anagn3@gmail.com>
2025-09-22 17:40:12 -0300

    tela de cadastro criada

--- lib/src/view/screens/startup_screen.dart
+++ lib/src/view/screens/startup_screen.dart
@@ -1,3 +1,4 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 
 class StartupScreen extends StatelessWidget {
@@ -6,46 +7,146 @@ class StartupScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.surface,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(style: TextStyle(fontSize: 38.0), 'Bem vindo ao'),
-                Text(
-                  style: TextStyle(
-                    fontSize: 38.0,
-                    color: Theme.of(context).colorScheme.primary,
+      body: Stack(
+        children: [
+          Container(color: Theme.of(context).colorScheme.surface),
+          Positioned(
+            top: 0,
+            right: 220,
+            child: ImageFiltered(
+              imageFilter: ImageFilter.blur(sigmaX: 70, sigmaY: 70),
+              child: Container(
+                width: 200,
+                height: 200,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  gradient: RadialGradient(
+                    colors: [
+                      Theme.of(context).colorScheme.primary,
+                      Colors.transparent,
+                    ],
+                    radius: 0.8,
                   ),
-                  "YggDrasil",
                 ),
-              ],
+              ),
             ),
-            Text("Monitore a vida e se junte a revolução verde"),
-            Image(image: AssetImage('assets/images/logo-yggdrasil.png')),
-            ElevatedButton(onPressed: () {}, child: Text("Criar Conta")),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+          ),
 
-              children: [
-                Text('Já tem uma conta?'),
-                TextButton(
-                  onPressed: () {},
-                  child: Text(
-                    style: TextStyle(
-                      decoration: TextDecoration.underline,
-                      color: Theme.of(context).colorScheme.primary,
-                    ),
-                    "Entrar",
+          Positioned(
+            bottom: 20,
+            left: 200,
+            child: ImageFiltered(
+              imageFilter: ImageFilter.blur(sigmaX: 100, sigmaY: 100),
+              child: Container(
+                width: 500,
+                height: 500,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  gradient: RadialGradient(
+                    colors: [
+                      Theme.of(context).colorScheme.primary,
+                      Colors.transparent,
+                    ],
+                    radius: 0.7,
                   ),
                 ),
-              ],
+              ),
             ),
-          ],
-        ),
+          ),
+
+          // Conteúdo da tela
+          Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: double.infinity,
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Bem vindo ao',
+                          style: TextStyle(
+                            fontSize: 38.0,
+                            fontWeight: FontWeight.bold,
+                            color: Theme.of(context).colorScheme.onSurface,
+                          ),
+                        ),
+                        Text(
+                          "YggDrasil",
+                          style: TextStyle(
+                            fontSize: 38.0,
+                            fontWeight: FontWeight.bold,
+                            color: Theme.of(context).colorScheme.primary,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  Text(
+                    "Monitore a vida e se junte à revolução verde",
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      color: Theme.of(context).colorScheme.outline,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Image.asset('assets/images/logo-yggdrasil.png'),
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                        shape: WidgetStateProperty.all(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+                        backgroundColor: WidgetStateProperty.all(
+                          Theme.of(context).colorScheme.primary,
+                        ),
+                      ),
+                      child: Text(
+                        "Criar Conta",
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.onSurface,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Já tem uma conta?',
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.onSurface,
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          "Entrar",
+                          style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            fontWeight: FontWeight.bold,
+                            color: Theme.of(context).colorScheme.primary,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
