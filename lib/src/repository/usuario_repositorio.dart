7f8b3ab9795460bd5a1d031aad96ae773476c9ca
LucasRossatto <lucas.sant.anagn3@gmail.com>
2025-09-25 17:27:16 -0300

    new methods

--- lib/src/repository/usuario_repositorio.dart
+++ lib/src/repository/usuario_repositorio.dart
@@ -1,11 +1,35 @@
-
 import 'package:yggdrasil_app/src/models/usuario_model.dart';
+import 'package:yggdrasil_app/src/models/wallet_model.dart';
 import 'package:yggdrasil_app/src/services/api_service.dart';
+
+class UsuarioResponse {
+  final UsuarioModel usuario;
+  final WalletModel wallet;
+  final int qtdeTagsTotal;
+
+  UsuarioResponse({
+    required this.usuario,
+    required this.wallet,
+    required this.qtdeTagsTotal,
+  });
+
+  factory UsuarioResponse.fromJson(Map<String, dynamic> json) {
+    return UsuarioResponse(
+      usuario: UsuarioModel.fromJson(json['usuario']),
+      wallet: WalletModel.fromJson(json['walllet']),
+      qtdeTagsTotal: json['qtdeTagsTotal'] ?? 0,
+    );
+  }
+}
 
 class UsuarioRepositorio {
   final ApiService _api = ApiService();
 
-  Future<UsuarioModel> cadastrarUsuario(String nome, String email, String senha) async {
+  Future<UsuarioModel> cadastrarUsuario(
+    String nome,
+    String email,
+    String senha,
+  ) async {
     final data = await _api.post("/CadastrarUsuario", {
       "nome": nome,
       "email": email,
@@ -14,10 +38,19 @@ class UsuarioRepositorio {
     return UsuarioModel.fromJson(data);
   }
 
-  Future<UsuarioModel> login(String email, String senha) async {
+  Future<int?> login(String email, String senha) async {
     final encodedEmail = Uri.encodeComponent(email);
     final encodedSenha = Uri.encodeComponent(senha);
     final data = await _api.get("/GetLogin/$encodedEmail,$encodedSenha");
-    return UsuarioModel.fromJson(data);
+    if (data['success'] == 1) {
+      return data['idUsuario'] as int;
+    }
+    return null;
+  }
+
+   Future<UsuarioResponse> getInformacoesUsuario(String id) async {
+    final encodedId = Uri.encodeComponent(id);
+    final data = await _api.get("/GetInformacoesBase/$encodedId");
+    return UsuarioResponse.fromJson(data);
   }
 }
--- lib/src/viewmodel/usuario_viewmodel.dart
+++ lib/src/viewmodel/usuario_viewmodel.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 import 'package:yggdrasil_app/src/models/usuario_model.dart';
+import 'package:yggdrasil_app/src/models/wallet_model.dart';
 import 'package:yggdrasil_app/src/repository/usuario_repositorio.dart';
 
 class UsuarioViewModel extends ChangeNotifier {
@@ -7,21 +9,25 @@ class UsuarioViewModel extends ChangeNotifier {
 
   bool isLoading = false;
   UsuarioModel? usuario;
+  WalletModel? wallet;
+  int? qtdeTagsTotal;
   String? erro;
 
-  Future<void> login(String email, String senha) async {
+  Future<int?> login(String email, String senha) async {
     isLoading = true;
     erro = null;
     notifyListeners();
 
     try {
-      usuario = await _repo.login(email, senha);
+      final usuario = await _repo.login(email, senha);
+      return usuario;
     } catch (e) {
       erro = e.toString();
     } finally {
       isLoading = false;
       notifyListeners();
     }
+    return null;
   }
 
   Future<void> cadastrarUsuario(String nome, String email, String senha) async {
@@ -37,5 +43,28 @@ class UsuarioViewModel extends ChangeNotifier {
       isLoading = false;
       notifyListeners();
     }
+  }
+
+  Future<UsuarioResponse?> getInformacoesUsuario(String id) async {
+    isLoading = true;
+    erro = null;
+    notifyListeners();
+
+    try {
+      final response = await _repo.getInformacoesUsuario(id);
+
+       usuario = response.usuario;
+       wallet = response.wallet;
+       qtdeTagsTotal = response.qtdeTagsTotal;
+
+       return response;
+
+    } catch (e) {
+      erro = e.toString();
+    } finally {
+      isLoading = false;
+      notifyListeners();
+    }
+    return null;
   }
 }
