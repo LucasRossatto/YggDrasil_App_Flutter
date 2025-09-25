c43b4798746d9aad1fb9aec5350f7fb3b04da76b
LucasRossatto <lucas.sant.anagn3@gmail.com>
2025-09-25 10:58:20 -0300

    adicionado wallet model

---
+++ lib/src/models/wallet_model.dart
@@ -0,0 +1,73 @@
+import 'package:equatable/equatable.dart';
+
+class WalletModel extends Equatable {
+  final String id;
+  final int usuarioId;
+  final String key;
+  final int yggCoin;
+  final int scc;
+  final int status;
+
+  const WalletModel({
+    required this.id,
+    required this.usuarioId,
+    required this.key,
+    required this.yggCoin,
+    required this.scc,
+    required this.status,
+  });
+
+  factory WalletModel.int() {
+    return WalletModel(
+      id: '',
+      usuarioId: 0,
+      key: '',
+      yggCoin: 0,
+      scc: 0,
+      status: 0,
+    );
+  }
+
+  factory WalletModel.fromJson(Map<String, dynamic> json) {
+    return WalletModel(
+      id: json['id'] ?? '',
+      usuarioId: json['usuarioId'] ?? '',
+      key: json['key'] ?? '',
+      yggCoin: json['yggCoin'] ?? '',
+      scc: json['scc'] ?? '',
+      status: json['status'] ?? '',
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'usuarioId': usuarioId,
+      'key': key,
+      'yggCoin': yggCoin,
+      'scc': scc,
+      'status': status,
+    };
+  }
+
+  WalletModel copyWith({
+    String? id,
+    int? usuarioId,
+    String? key,
+    int? yggCoin,
+    int? scc,
+    int? status,
+  }) {
+    return WalletModel(
+      id: id ?? this.id,
+      usuarioId: usuarioId ?? this.usuarioId,
+      key: key ?? this.key,
+      yggCoin: yggCoin ?? this.yggCoin,
+      scc: scc ?? this.scc,
+      status: status ?? this.status,
+    );
+  }
+
+  @override
+  List<Object?> get props => [id, usuarioId, key, yggCoin, scc, status];
+}
