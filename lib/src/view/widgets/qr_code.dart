40c4407d59a5ef23ab859115622722ca4b88f6f2
LucasRossatto <lucas.sant.anagn3@gmail.com>
2025-09-27 22:04:51 -0300

    qr conde funciona

--- lib/src/view/widgets/qr_code.dart
+++ lib/src/view/widgets/qr_code.dart
@@ -2,20 +2,35 @@ import 'package:flutter/material.dart';
 import 'package:pretty_qr_code/pretty_qr_code.dart';
 
 class WalletQrCode extends StatefulWidget {
-  const WalletQrCode({super.key});
+  final String walletKey;
+
+  const WalletQrCode({super.key, required this.walletKey});
 
   @override
-  State<WalletQrCode> createState() => _WalletQrCode();
+  State<WalletQrCode> createState() => _WalletQrCodeState();
 }
 
-class _WalletQrCode extends State<WalletQrCode> {
+class _WalletQrCodeState extends State<WalletQrCode> {
   @override
   Widget build(BuildContext context) {
-    return PrettyQrView.data(
-      data: 'carteira ygg',
-      decoration: const PrettyQrDecoration(
-        image: PrettyQrDecorationImage(image: AssetImage('assets/images/logo-yggdrasil-qrcode.png')),
-        quietZone: PrettyQrQuietZone.standart,
+    final double qrSize = MediaQuery.of(context).size.width - 140;
+    final String walletKey = widget.walletKey;
+
+    return Center(
+      child: SizedBox(
+        width: qrSize,
+        height: qrSize,
+        child: PrettyQrView.data(
+          data: walletKey,
+          errorCorrectLevel: QrErrorCorrectLevel.H,
+          decoration: const PrettyQrDecoration(
+            quietZone: PrettyQrQuietZone.zero,
+            image: PrettyQrDecorationImage(
+              position: PrettyQrDecorationImagePosition.embedded,
+              image: AssetImage('assets/images/logo-yggdrasil.png'),
+            ),
+          ),
+        ),
       ),
     );
   }
