c839d3ebb6b821a6dc9719d435af73dd15b626dd
LucasRossatto <lucas.sant.anagn3@gmail.com>
2025-09-27 22:04:42 -0300

    feat wallet passada com mock

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:yggdrasil_app/src/models/usuario_model.dart';
+import 'package:yggdrasil_app/src/models/wallet_model.dart';
 import 'package:yggdrasil_app/src/states/usuario_state.dart';
 import 'package:yggdrasil_app/src/view/screens/home_screen.dart';
 import 'src/shared/themes/theme.dart';
@@ -32,12 +33,21 @@ class MyApp extends StatelessWidget {
       nome: "Lucas Rossatto",
       email: "lucas.rossatto@example.com",
     );
+
+    final walletMock = WalletModel(
+      id: 1,
+      usuarioId: 101,
+      key: "mocked-key-123",
+      yggCoin: 500,
+      scc: 250,
+      status: 1, // exemplo: 1 = ativo, 0 = inativo
+    );
     return MaterialApp(
       title: 'YggDrasil Demo',
       themeMode: ThemeMode.light,
       theme: theme.light(),
       darkTheme: theme.dark(),
-      home: HomeScreen(usuario: usuarioMock),
+      home: HomeScreen(usuario: usuarioMock, wallet: walletMock),
     );
   }
 }
