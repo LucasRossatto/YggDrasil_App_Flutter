2235f6a71f24d7b4bd2abe64c78f91eccdb02fd9
LucasRossatto <lucas.sant.anagn3@gmail.com>
2025-09-29 00:30:37 -0300

    feaat: model de arvore adicionado

---
+++ lib/src/models/arvore_model.dart
@@ -0,0 +1,90 @@
+import 'package:equatable/equatable.dart';
+
+class ArvoreModel extends Equatable {
+  final int usuarioId;
+  final int tagId;
+  final String imagemURL;
+  final String nome;
+  final String familia;
+  final String idadeAproximada;
+  final String localizacao;
+  final int nota;
+  final int tipo;
+
+  const ArvoreModel({
+    required this.usuarioId,
+    required this.tagId,
+    required this.imagemURL,
+    required this.nome,
+    required this.familia,
+    required this.idadeAproximada,
+    required this.localizacao,
+    required this.nota,
+    required this.tipo,
+  });
+
+  factory ArvoreModel.fromJson(Map<String, dynamic> json) {
+    return ArvoreModel(
+      usuarioId: json['usuarioId'] ?? 0,
+      tagId: json['tagId'] ?? 0,
+      imagemURL: json['imagemURL'] ?? '',
+      nome: json['nome'] ?? '',
+      familia: json['familia'] ?? '',
+      idadeAproximada: json['idadeAproximada'] ?? '',
+      localizacao: json['localizacao'] ?? '',
+      nota: json['nota'] ?? 0,
+      tipo: json['tipo'] ?? 0,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'usuarioId': usuarioId,
+      'tagId': tagId,
+      'imagemURL': imagemURL,
+      'nome': nome,
+      'familia': familia,
+      'idadeAproximada': idadeAproximada,
+      'localizacao': localizacao,
+      'nota': nota,
+      'tipo': tipo,
+    };
+  }
+
+  ArvoreModel copyWith({
+    int? usuarioId,
+    int? tagId,
+    String? imagemURL,
+    String? nome,
+    String? familia,
+    String? idadeAproximada,
+    String? localizacao,
+    int? nota,
+    int? tipo,
+  }) {
+    return ArvoreModel(
+      usuarioId: usuarioId ?? this.usuarioId,
+      tagId: tagId ?? this.tagId,
+      imagemURL: imagemURL ?? this.imagemURL,
+      nome: nome ?? this.nome,
+      familia: familia ?? this.familia,
+      idadeAproximada: idadeAproximada ?? this.idadeAproximada,
+      localizacao: localizacao ?? this.localizacao,
+      nota: nota ?? this.nota,
+      tipo: tipo ?? this.tipo,
+    );
+  }
+
+  @override
+  List<Object?> get props => [
+        usuarioId,
+        tagId,
+        imagemURL,
+        nome,
+        familia,
+        idadeAproximada,
+        localizacao,
+        nota,
+        tipo,
+      ];
+}
