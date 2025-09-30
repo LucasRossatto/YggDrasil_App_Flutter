9d82376a8511d1f5d1b99bcf336008076c431110
LucasRossatto <lucas.sant.anagn3@gmail.com>
2025-09-30 17:46:53 -0300

    primeiras infos passadas

--- lib/src/view/screens/detalhe_arvore_screen.dart
+++ lib/src/view/screens/detalhe_arvore_screen.dart
@@ -7,6 +7,33 @@ class DetalheArvoreScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SingleChildScrollView();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Detalhes da Árvore"),
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromRGBO(0, 166, 62, 1),
+                Color.fromRGBO(0, 122, 85, 1),
+              ],
+              stops: [0, 0.5],
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+            ),
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Text(arvore.nome),
+            Text(arvore.tipo.toString()),
+            Text(arvore.familia),
+            Text(arvore.idadeAproximada),
+          ],
+        ),
+      ),
+    );
   }
 }
