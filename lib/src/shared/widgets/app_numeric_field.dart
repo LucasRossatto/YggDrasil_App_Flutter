04b3b539f1c03fd607b96c993d502b2a6d3471cc
LucasRossatto <lucas.sant.anagn3@gmail.com>
2025-10-02 16:21:24 -0300

    feat: novo campo para números

---
+++ lib/src/shared/widgets/app_numeric_field.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
+
+class AppNumericField extends StatelessWidget {
+  final String label;
+  final String? hint;
+  final String? extraLabel;
+  final TextEditingController? controller;
+  final bool obscureText;
+  final String? Function(String?)? validator;
+  final Widget? prefixIcon;
+  final Widget? suffixIcon;
+  final bool enabled;
+  final formatter = NumberFormat( "#,##0.###" );
+
+  AppNumericField({
+    super.key,
+    required this.label,
+    this.hint,
+    this.extraLabel,
+    this.controller,
+    this.obscureText = false,
+    this.validator,
+    this.prefixIcon,
+    this.suffixIcon,
+    this.enabled = true,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        if (extraLabel != null) ...[
+          Text(
+            extraLabel!,
+            style: theme.textTheme.bodyMedium?.copyWith(
+              fontWeight: FontWeight.bold,
+              color: theme.colorScheme.onSurface,
+            ),
+          ),
+          const SizedBox(height: 6), // Espaçamento entre label e campo
+        ],
+        TextFormField(
+          controller: controller,
+          keyboardType: TextInputType.number,
+          inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+          obscureText: obscureText,
+          validator: validator,
+          enabled: enabled,
+          decoration: InputDecoration(
+            labelText: label,
+            hintText: hint,
+            prefixIcon: prefixIcon,
+            suffixIcon: suffixIcon,
+            filled: true,
+            fillColor: enabled
+                ? theme.colorScheme.surface
+                : theme.colorScheme.onSurfaceVariant,
+            labelStyle: theme.textTheme.bodyMedium,
+            hintStyle: theme.textTheme.bodySmall?.copyWith(
+              color: theme.hintColor,
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12),
+              borderSide:  BorderSide(color: theme.colorScheme.outline,),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12),
+              borderSide: BorderSide(color: theme.colorScheme.outline),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12),
+              borderSide: BorderSide(color: theme.colorScheme.primary, width: 2),
+            ),
+            errorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12),
+              borderSide: BorderSide(color: theme.colorScheme.error),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
