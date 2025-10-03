c6c12c50ab6c929a2e3f7d7ec57c3f38f7d2cf97
LucasRossatto <lucas.sant.anagn3@gmail.com>
2025-10-03 15:25:45 -0300

    feat: validações com validator

--- lib/src/view/widgets/adicionar_arvore_form.dart
+++ lib/src/view/widgets/adicionar_arvore_form.dart
@@ -57,124 +57,161 @@ class _ArvoreCreateFormState extends State<ArvoreCreateForm> {
     final screenHeight = MediaQuery.of(context).size.height;
     final paddingHorizontal = screenWidth * 0.05; // 5% da largura da tela
     final spacingVertical = screenHeight * 0.02; // 2% da altura da tela
+    final _formKey = GlobalKey<FormState>();
 
     return SingleChildScrollView(
       padding: EdgeInsets.symmetric(
         horizontal: paddingHorizontal,
         vertical: spacingVertical,
       ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(height: spacingVertical),
-          Text(
-            "Tag",
-            style: theme.textTheme.bodyMedium?.copyWith(
-              fontWeight: FontWeight.bold,
-              color: theme.colorScheme.onSurface,
-            ),
-          ),
-          const SizedBox(height: 6),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Expanded(
-                flex: 3,
-                child: AppTextField(
-                  controller: widget.tagIdController,
-                  label: "Digite ou escaneiro o QR code",
-                  hint: "Digite a Tag da árvore ou escaneie o QR Code",
-                ),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: spacingVertical),
+            Text(
+              "Tag",
+              style: theme.textTheme.bodyMedium?.copyWith(
+                fontWeight: FontWeight.bold,
+                color: theme.colorScheme.onSurface,
               ),
-              SizedBox(width: screenWidth * 0.03),
-              Expanded(
-                flex: 1,
-                child: IconButton(
-                  onPressed: widget.abrirScanner,
-                  iconSize: screenWidth * 0.08,
-                  icon: SvgPicture.asset(
-                    'assets/Icons/Icone_YGGTAGG.svg',
-                    // ignore: deprecated_member_use
-                    color: Theme.of(context).colorScheme.surface,
-                    width: screenWidth * 0.08,
-                    height: screenWidth * 0.08,
+            ),
+            const SizedBox(height: 6),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(
+                  flex: 3,
+                  child: AppTextField(
+                    controller: widget.tagIdController,
+                    label: "Digite ou escaneiro o QR code",
+                    hint: "Digite a Tag da árvore ou escaneie o QR Code",
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Informe uma tag válida";
+                      }
+                      return null;
+                    },
                   ),
-                  style: ButtonStyle(
-                    shape: WidgetStatePropertyAll(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8),
+                ),
+                SizedBox(width: screenWidth * 0.03),
+                Expanded(
+                  flex: 1,
+                  child: IconButton(
+                    onPressed: widget.abrirScanner,
+                    iconSize: screenWidth * 0.08,
+                    icon: SvgPicture.asset(
+                      'assets/Icons/Icone_YGGTAGG.svg',
+                      // ignore: deprecated_member_use
+                      color: Theme.of(context).colorScheme.surface,
+                      width: screenWidth * 0.08,
+                      height: screenWidth * 0.08,
+                    ),
+                    style: ButtonStyle(
+                      shape: WidgetStatePropertyAll(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                      backgroundColor: WidgetStatePropertyAll(
+                        Theme.of(context).colorScheme.primary,
                       ),
                     ),
-                    backgroundColor: WidgetStatePropertyAll(
-                      Theme.of(context).colorScheme.primary,
-                    ),
                   ),
                 ),
-              ),
-            ],
-          ),
-          SizedBox(height: spacingVertical),
-          AppTextField(
-            controller: nomeController,
-            label: "Nome",
-            hint: "Nome da árvore",
-            extraLabel: "Nome",
-          ),
-          SizedBox(height: spacingVertical),
-          AppTextField(
-            controller: familiaController,
-            label: "Família",
-            hint: "Família da árvore (Nome Científico)",
-            extraLabel: "Família",
-          ),
-          SizedBox(height: spacingVertical),
-          AppNumericField(
-            controller: idadeAproximadaController,
-            label: "Idade Aproximada",
-            hint: "Idade da árvore",
-            extraLabel: "Idade",
-          ),
-          AppTextField(
-            controller: historicoController,
-            label: "Histórico",
-            hint: "Digite o histórico da árvore",
-            extraLabel: "Histórico",
-          ),
-          SizedBox(height: spacingVertical),
-          SizedBox(
-            width: double.infinity,
-            child: TransferirButton(
-              text: "Criar Árvore",
-              onPressed: () {
-                if (widget.tagIdController.text.isEmpty ||
-                    nomeController.text.isEmpty ||
-                    familiaController.text.isEmpty ||
-                    historicoController.text.isEmpty) {
-                  CustomSnackBar.show(
-                    context,
-                    message: "Preencha todos os campos obrigatórios.",
-                    icon: Icons.error,
-                    backgroundColor: theme.colorScheme.errorContainer,
-                  );
-                  return;
+              ],
+            ),
+            SizedBox(height: spacingVertical),
+            AppTextField(
+              controller: nomeController,
+              label: "Nome",
+              hint: "Nome da árvore",
+              extraLabel: "Nome",
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return "Informe um nome para a árvore";
                 }
-
-                final arvoreAtualizada = widget.arvore.copyWith(
-                  tagId: widget.tagIdController.text,
-                  imagemURL: '',
-                  nome: nomeController.text,
-                  familia: familiaController.text,
-                  idadeAproximada: idadeAproximadaController.text,
-                  nota: 1,
-                  tipo: 1,
-                  mensagem: historicoController.text,
-                );
-
-                widget.onSubmit(arvoreAtualizada);
+                return null;
               },
             ),
-          ),
-        ],
+            SizedBox(height: spacingVertical),
+            AppTextField(
+              controller: familiaController,
+              label: "Família",
+              hint: "Família da árvore (Nome Científico)",
+              extraLabel: "Família",
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return "Informe uma família para a árvore";
+                }
+                return null;
+              },
+            ),
+            SizedBox(height: spacingVertical),
+            AppNumericField(
+              controller: idadeAproximadaController,
+              label: "Idade Aproximada",
+              hint: "Idade da árvore",
+              extraLabel: "Idade",
+              validator: (value) {
+                if (value == null || value == 0 || value.isEmpty) {
+                  return "Informe uma idade para a árvore";
+                }
+                return null;
+              },
+            ),
+            AppTextField(
+              controller: historicoController,
+              label: "Histórico",
+              hint: "Digite o histórico da árvore",
+              extraLabel: "Histórico",
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return "Informe um histórico para a árvore";
+                }
+                return null;
+              },
+            ),
+            SizedBox(height: spacingVertical),
+            SizedBox(
+              width: double.infinity,
+              child: TransferirButton(
+                text: "Criar Árvore",
+                onPressed: () {
+                  if (!_formKey.currentState!.validate()) {
+                    return;
+                  }
+                  if (widget.tagIdController.text.isEmpty ||
+                      nomeController.text.isEmpty ||
+                      familiaController.text.isEmpty ||
+                      historicoController.text.isEmpty) {
+                    CustomSnackBar.show(
+                      context,
+                      message: "Preencha todos os campos obrigatórios.",
+                      icon: Icons.error,
+                      backgroundColor: theme.colorScheme.errorContainer,
+                    );
+                    return;
+                  }
+
+                  final arvoreAtualizada = widget.arvore.copyWith(
+                    tagId: widget.tagIdController.text,
+                    imagemURL: '',
+                    nome: nomeController.text,
+                    familia: familiaController.text,
+                    idadeAproximada: idadeAproximadaController.text,
+                    nota: 1,
+                    tipo: 1,
+                    mensagem: historicoController.text,
+                  );
+
+                  widget.onSubmit(arvoreAtualizada);
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
