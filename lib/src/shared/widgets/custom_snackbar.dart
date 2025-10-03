446c4e19473c1d7b8526a6208f96131e7ffb19d8
LucasRossatto <lucas.sant.anagn3@gmail.com>
2025-10-03 14:14:53 -0300

    feat: perfis de estilização

--- lib/src/shared/widgets/custom_snackbar.dart
+++ lib/src/shared/widgets/custom_snackbar.dart
@@ -1,16 +1,29 @@
 import 'package:flutter/material.dart';
 
 class CustomSnackBar {
+  
+  static const _profiles = {
+    "success": {"color": Colors.green, "icon": Icons.check_circle},
+    "error": {"color": Color(0xFFFF5449), "icon": Icons.error_outline_rounded},
+    "warning": {"color": Colors.amber, "icon": Icons.warning_amber_rounded},
+  };
+
   static void show(
     BuildContext context, {
     required String message,
-    IconData icon = Icons.check_circle,
-    Color backgroundColor = Colors.green,
+    String profile = "success",
+    IconData? icon,
+    Color? backgroundColor,
   }) {
+
+    final profileData = _profiles[profile] ?? _profiles["success"]!;
+    final snackBarIcon = icon ?? profileData["icon"] as IconData;
+    final snackBarColor = backgroundColor ?? profileData["color"] as Color;
+
     final snackBar = SnackBar(
       content: Row(
         children: [
-          Icon(icon, color: Colors.white),
+          Icon(snackBarIcon, color: Colors.white),
           const SizedBox(width: 12),
           Expanded(
             child: Text(
@@ -20,7 +33,7 @@ class CustomSnackBar {
           ),
         ],
       ),
-      backgroundColor: backgroundColor,
+      backgroundColor: snackBarColor,
       behavior: SnackBarBehavior.floating,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(12),
